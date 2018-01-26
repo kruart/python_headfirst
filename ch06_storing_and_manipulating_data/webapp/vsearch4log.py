0fc762db7281867f47d0ce7318ef38792469e5e0
we0z <weoz@ukr.net>
2018-01-26 13:50:21 +0200

    ch06: write logs in file

---
+++ ch06_storing_and_manipulating_data/webapp/vsearch4log.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, escape
+from ch04_functions_modules.vsearch import search4letters
+
+app = Flask(__name__)
+
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+
+    log_request(request, results)
+
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    with open('vsearch.log', 'a') as logfile:
+        print(req, res, file=logfile)
+
+if __name__ == '__main__':
+    app.run(debug=True)

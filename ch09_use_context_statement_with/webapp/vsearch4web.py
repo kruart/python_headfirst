995392ec020f2dd46146a8d3f89ec963deb57f20
we0z <weoz@ukr.net>
2018-01-28 14:05:40 +0200

    ch09: change log_request(): use with statement

--- ch09_use_context_statement_with/webapp/vsearch4web.py
+++ ch09_use_context_statement_with/webapp/vsearch4web.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template, request, escape
 from ch04_functions_modules.vsearch import search4letters
-import mysql.connector
+from ch09_use_context_statement_with.webapp.DBcm import UseDatabase
 
 app = Flask(__name__)
+
+dbconfig = {'host': '127.0.0.1',
+                          'user': 'vsearch',
+                          'password': 'vsearchpasswd',
+                          'database': 'vsearchlogDB', }
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+
+    with UseDatabase(dbconfig) as cursor:
+        _SQL = """insert into log (phrase, letters, ip, browser_string, results) values(%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res))
 
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+    """Extract the posted data; perform the search; return results."""
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
@@ -24,23 +38,13 @@ def do_search() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
-
-
-def log_request(req: 'flask_request', res: str) -> None:
-    """Log details of the web request and the results."""
-    dbconfig = {'host': '127.0.0.1', 'user': 'vsearch', 'password': 'vsearchpasswd', 'database': 'vsearchlogDB', }
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log (phrase, letters, ip, browser_string, results) values(%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (request.form['phrase'], request.form['letters'], request.remote_addr, request.user_agent.browser, res))
-    conn.commit()
-    cursor.close()
-    conn.close()
 
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
+    """Display the contents of the log file as a HTML table."""
     contents = []   # contains lists in list
     with open('vsearch.log') as logfile:
         for line in logfile:
